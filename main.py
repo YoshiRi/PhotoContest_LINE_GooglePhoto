61c21411dcd3528a5ee6f93aa34fd3ac92e86263
YoshiRi <yoshiyoshidetteiu@gmail.com>
2022-07-19 17:32:28 +0900

    add db create to make database work properly

--- main.py
+++ main.py
@@ -20,6 +20,8 @@ import json
 from io import BytesIO
 from argparse import ArgumentParser
 import urllib.request
+import traceback
+
 
 from flask import Flask, request, abort, g
 from linebot import LineBotApi, WebhookHandler
@@ -59,6 +61,10 @@ app.config['SQLALCHEMY_DATABASE_URI'] = sqlalchemy_database_uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
 db.init_app(app)
+
+# database生成に必要だった
+with app.app_context():
+    db.create_all()
 
 # line bot api
 line_bot_api = LineBotApi(channel_access_token)
@@ -103,12 +109,16 @@ def message_text(event):
             line_bot_api.reply_message(
                 event.reply_token,
                 TextMessage(text="Bot can't use profile API without user ID"))
-    elif text == 'あいいい':
+    elif text == 'echo':
         line_bot_api.reply_message(
-            event.reply_token, TextSendMessage(text='あいいいいいいいい'))
+            event.reply_token, TextSendMessage(text='エコー'))
+    elif text == 'access token':
+        gphoto_access_token = photo.get_gphoto_access_token()
+        print(gphoto_access_token)
     else:
-        line_bot_api.reply_message(
-            event.reply_token, TextSendMessage(text='テキストの返信は設定していません。写真のみ対応しています。'))
+        print("Nothing")
+    #    line_bot_api.reply_message(
+    #        event.reply_token, TextSendMessage(text='テキストの返信は設定していません。写真のみ対応しています。'))
 
 
 """ BOTと友達になったとき
@@ -139,32 +149,32 @@ def handle_image_message(event):
 
     # ユーザーのこれまでの投稿枚数を取得
     counter = get_user_counter(user_id, display_name)
-    line_bot_api.reply_message(
-        event.reply_token, TextSendMessage(text=str(counter+1)+'枚目の写真の投稿を受領しました。'))
+    print(counter)
     try:
 
         # 写真の投稿枚数の上限を超えている場合
         if counter >= const.PHOTO_POST_LIMIT:
             counter += 1
-            line_bot_api.reply_message(
-                event.reply_token, TextSendMessage(text='すみません。'+str(const.PHOTO_POST_LIMIT)+'枚以降の投稿はできません。'))
+            # line_bot_api.reply_message(
+            #    event.reply_token, TextSendMessage(text='すみません。'+str(const.PHOTO_POST_LIMIT)+'枚以降の投稿はできません。'))
             raise PhotoPostLimitationError
 
         """ 画像をGooglePhotoへアップロード [ここから] """
         # LINEサーバーから画像取得
         try:
-            img_data = photo.get_photo_data(msg_id=event.message.id)
-        except:
-            line_bot_api.reply_message(
-                event.reply_token, TextSendMessage(text='画像取得失敗です'))
+            print("Extracting Image...")
+            img_content = line_bot_api.get_message_content(event.message.id)
+            img_data = BytesIO(img_content.content).read()
+            print(img_data)
+        except Exception as e:
+            print("Loading Line Message:", e)
             raise ValueError('LINEサーバーから画像の取得に失敗')
 
         # Google Photoのアクセストークンを取得
         try:
             gphoto_access_token = photo.get_gphoto_access_token()
-        except:
-            line_bot_api.reply_message(
-                event.reply_token, TextSendMessage(text='アクセストークン取得失敗'))
+        except Exception as e:
+            print("Error at get access token", e)
             raise ValueError('Google Photoのアクセストークンを取得に失敗')
 
         # Google Photoへ画像をアップロード
@@ -172,14 +182,14 @@ def handle_image_message(event):
             upload_token = photo.get_gphoto_upload_token(
                 gphoto_access_token, img_data, display_name + '_' + str(counter+1))
         except:
-            line_bot_api.reply_message(
-                event.reply_token, TextSendMessage(text='アップロード失敗'))
+            traceback.print_exc()
             raise ValueError('Google Photoへ画像をアップロードに失敗')
 
         # 画像をGoogle Photoアルバムに追加
         try:
             photo.upload_photo(gphoto_access_token, upload_token)
         except:
+            traceback.print_exc()
             raise ValueError('画像をGoogle Photoアルバムに追加に失敗')
         """ 画像をGooglePhotoへアップロード [ここまで] """
 
@@ -192,11 +202,13 @@ def handle_image_message(event):
     except PhotoPostLimitationError:
         msg = message_for_posting_a_photo(display_name, counter)
     # それ以外のエラー
-    except:
+    except Exception as e:
+        print(e)
         msg = const.UPLOAD_FAILED_MESSAGE
 
     # メッセージを返却
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=msg))
+    print(msg)
+    #line_bot_api.reply_message(event.reply_token, TextSendMessage(text=msg))
 
 
 """ スタンプが送られた場合
